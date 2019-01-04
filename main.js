b8506af5a414d019838414030808e5a54acbcc4f
mdif <710740608@qq.com>
2019-01-04 23:46:58 +0800

    AJAX API Ver1.0.0

---
+++ main.js
@@ -0,0 +1,52 @@
+window.jQuery = function (nodesOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () { }
+    nodes.removeClass = function () { }
+    return nodes
+}
+window.$ = window.jQuery
+
+function f1(responseText){
+    console.log(responseText)
+}
+function f2(responseText){
+    console.log(responseText)
+}
+testBtn.onclick = function () {
+    $.ajax(
+        {
+            url: '/xxx',
+            method: 'POST',
+            body: 'a=1&b=2',
+            successFn: (responseText) => { console.log('我是成功后的回调函数');f1(responseText);f2(responseText);},
+            failFn: () => { console.log('我是失败后的回调函数') },
+            header: { 'yang': 18, 'Content-Type': 'application/x-www-form-urlencoded' }
+        })
+}
+window.jQuery.ajax = function (obj) {
+    let url = obj.url
+    let method = obj.method
+    let body = obj.body
+    let successFn = obj.successFn
+    let failFn = obj.failFn
+    let header = obj.header
+    console.log(obj)
+    let request = new XMLHttpRequest()
+    request.open(method, url)
+    for (let key in header) {
+        let value = header[key]
+        request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                console.log('请求成功')
+                successFn.call(undefined,request.responseText)
+            } else if (request.status >= 400) {
+                console.log('请求失败')
+                failFn.call(undefined,request.responseText)
+            }
+        }
+    }
+    request.send(body)
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+  if(path === '/'){
+    let string = fs.readFileSync('./index.html','utf-8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){
+    let string = fs.readFileSync('./main.js','utf-8')
+    response.statusCode = 200
+    response.setHeader('Content-Type','text/javascript;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/xxx'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/json;charset=utf-8')
+    response.write(`
+      {
+        "note":{
+          "to":"1年后的我",
+          "head":"一些话",
+          "body":"加油，生活会慢慢变好的",
+          "form":"杨子璇"
+        }
+      }
+    `)
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'image/jpg;charset=utf-8')
+    response.write('error了')
+    response.end()
+  }
+ 
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
